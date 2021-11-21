a7acb1c60b974649fcf5bd38a4aae4b9c1536c1c
LeoAsher25 <leoasher2507@gmail.com>
2021-11-21 15:37:11 +0700

    fix max length

--- store/models.py
+++ store/models.py
@@ -21,7 +21,7 @@ class Product(models.Model):
     price = models.IntegerField()
     images = models.ImageField(upload_to='photos/products')
     stock = models.IntegerField()
-    note = models.CharField(max_length=500, null=True)
+    note = models.CharField(max_length=1000, null=True)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
     created_date = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now=True)
